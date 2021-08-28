b95322c89b6f784423ace562c126522ca8005fce
Christopher Bentzrud <cbentzrud@gmail.com>
2021-08-28 18:53:19 +0200

    update to new source

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import urllib.request
 import discord
 import os
-from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 client = discord.Client()
 
 def getPriceList():
 	try:
-		with urllib.request.urlopen('https://starbase.auction/livefeed.txt') as f:
+		with urllib.request.urlopen('https://docs.google.com/spreadsheets/d/19Ce3veTJyVLm_qoAqV4QWIJNODuXwLbvv2GhGB37Nkg/edit#gid=1157372011') as f:
 			priceStr = f.read().decode('utf-8')
 	except urllib.error.URLError as e:
 		print(e.reason)
 		return []
-	return list(map(lambda x: [x.split(":")[0].strip(), x.split(":")[1].split("-")[0].strip(), x.split(":")[1].split("-")[1].strip()], priceStr.split("|")))
+	soup = BeautifulSoup(priceStr, "lxml")
+	tables = soup.find_all("table")
+	formatted = list(list(list(td.text for td in row.find_all("td")) for row in table.find_all("tr")) for table in tables)
+	orePrices = formatted[0][3][33:48]
+	return orePrices
 
 @client.event
 async def on_ready():
@@ -25,14 +30,13 @@ async def on_message(message):
 		return
 	if message.content.startswith('!sbprices'):
 		pList = getPriceList()
+		oList = ['Kutonium', 'Arkanium', 'Valkite', 'Nhurgite', 'Karnite','Vokarium','Charodium','Ajatite','Bastium','Exorium','Aegisium','Ice','Ymrite','Surtrite','Corazium']
 		if len(pList) > 0:
-			ores = "\n".join(list(map(lambda x: x[0], pList)))
+			ores = "\n".join(list(map(lambda x: x[0], oList)))
 			prices = "\n".join(list(map(lambda x: x[1], pList)))
-			available = "\n".join(list(map(lambda x: x[2], pList)))
 			embed=discord.Embed(title="Current Starbase Auction Prices", color=0xff2626)
 			embed.add_field(name="Ore", value=ores, inline=True)
 			embed.add_field(name="Price", value=prices, inline=True)
-			embed.add_field(name="Available", value=available, inline=True)
 			embed.set_footer(text="From starbase.auction/livefeed.txt")
 			await message.channel.send(embed=embed)
 		else:
