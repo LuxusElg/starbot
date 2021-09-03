acb76278c5dd4d368104b28d782c337a8b0a7e56
Christopher <cbentzrud@gmail.com>
2021-09-03 11:11:17 +0200

    changed to scrape from correct worksheet

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import urllib.request
 import discord
 import os
-from bs4 import BeautifulSoup
-#from dotenv import load_dotenv
+import csv
+from io import StringIO
 
-#load_dotenv()
 client = discord.Client()
 
 def getPriceList():
 	try:
-		with urllib.request.urlopen('https://docs.google.com/spreadsheets/d/19Ce3veTJyVLm_qoAqV4QWIJNODuXwLbvv2GhGB37Nkg/edit#gid=1157372011') as f:
+		with urllib.request.urlopen('https://docs.google.com/spreadsheets/d/19Ce3veTJyVLm_qoAqV4QWIJNODuXwLbvv2GhGB37Nkg/gviz/tq?tqx=out:csv&sheet=%20') as f:
 			priceStr = f.read().decode('utf-8')
 	except urllib.error.URLError as e:
 		print(e.reason)
 		return []
-	soup = BeautifulSoup(priceStr, "lxml")
-	tables = soup.find_all("table")
-	formatted = list(list(list(td.text for td in row.find_all("td")) for row in table.find_all("tr")) for table in tables)
-	orePrices = formatted[0][3][35:50]
+	f = StringIO(priceStr)
+	reader = csv.reader(f, delimiter=',')
+	formatted = list(row[0:4] for row in reader)
+	orePrices = formatted[1:17]
 	return orePrices
 
 @client.event
@@ -30,13 +29,12 @@ async def on_message(message):
 		return
 	if message.content.startswith('!sbprices'):
 		pList = getPriceList()
-		oList = ['Kutonium', 'Arkanium', 'Valkite', 'Nhurgite', 'Karnite','Vokarium','Charodium','Ajatite','Bastium','Exorium','Aegisium','Ice','Ymrite','Surtrite','Corazium']
 		if len(pList) > 0:
-			ores = "\n".join(oList)
-			prices = "\n".join(pList)
 			embed=discord.Embed(title="Current Starbase Auction Prices", color=0xff2626)
-			embed.add_field(name="Ore", value=ores, inline=True)
-			embed.add_field(name="Price", value=prices, inline=True)
+			embed.add_field(name="Ore", value='\n'.join(ore[0] for ore in pList), inline=True)
+			embed.add_field(name="Price", value='\n'.join(ore[1] for ore in pList), inline=True)
+			embed.add_field(name="Available", value='\n'.join(ore[2] for ore in pList), inline=True)
+			embed.add_field(name="% of NPC", value='\n'.join(ore[3] for ore in pList), inline=True)
 			embed.set_footer(text="From Septic's Google Sheet")
 			await message.channel.send(embed=embed)
 		else:
